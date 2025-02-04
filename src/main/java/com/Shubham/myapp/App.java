6f35c5dae7a31466bae1edf8481e76ed07aef13e
Shubham <shubhamk345@gmail.com>
2025-02-04 09:57:14 +0000

    initial commit

--- src/main/java/com/Shubham/myapp/App.java
+++ src/main/java/com/Shubham/myapp/App.java
@@ -5,7 +5,7 @@ package com.Shubham.myapp;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello World!";
+    private static final String MESSAGE = "Hello World!!!!";
 
     public App() {}
 
