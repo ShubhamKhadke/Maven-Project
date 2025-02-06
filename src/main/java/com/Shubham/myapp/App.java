f16f63bc1285016b568c03c659a47ebc2def6e62
Shubham Khadke <157566731+ShubhamKhadke@users.noreply.github.com>
2025-02-06 12:25:12 +0530

    Update App.java

--- src/main/java/com/Shubham/myapp/App.java
+++ src/main/java/com/Shubham/myapp/App.java
@@ -1,19 +1,12 @@
-package com.Shubham.myapp;
+package com.example;
 
-/**
- * Hello world!
- */
 public class App {
-
-    private static final String MESSAGE = "Hello World!!!!";
-
-    public App() {}
-
-    public static void main(String[] args) {
-        System.out.println(MESSAGE);
+    public String greet(String name) {
+        return "Hello, " + name;
     }
 
-    public String getMessage() {
-        return MESSAGE;
+    public static void main(String[] args) {
+        App app = new App();
+        System.out.println(app.greet("World"));
     }
 }
